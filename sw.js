13fec7fd59929409fce29acc2311582bd921de67
Asuna-hub-web <bernaliaedoumou@gmail.com>
2026-09-20 08:17:18 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE = 'ma-caisse-v1';
+const CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(req, { ignoreSearch: true }).then((hit) => {
+        const net = fetch(req)
+          .then((res) => {
+            if (res && res.ok) { const copy = res.clone(); caches.open(CACHE).then((c) => c.put(req, copy)); }
+            return res;
+          })
+          .catch(() => hit || caches.match('./index.html'));
+        return hit || net;
+      })
+    );
+    return;
+  }
+
+  if (url.hostname === 'fonts.googleapis.com' || url.hostname === 'fonts.gstatic.com') {
+    e.respondWith(
+      caches.open(CACHE).then((c) =>
+        c.match(req).then((hit) => {
+          const net = fetch(req).then((res) => { c.put(req, res.clone()); return res; }).catch(() => hit);
+          return hit || net;
+        })
+      )
+    );
+  }
+});
